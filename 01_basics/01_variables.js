8498a3922e4db8ada9f9a15311878668b099dc3f
Himesh000 <138201789+Himesh000@users.noreply.github.com>
2025-05-12 09:29:29 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 123123;
+let accountEmail = "himesh@google.com";
+var accountPassword = 12345;
+let accountState;
+
+console.table([accountId , accountEmail , accountPassword , accountState])
+
