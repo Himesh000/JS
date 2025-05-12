34079aeba7e79fc2954d195b480a57ed6cc2c82d
Himesh000 <138201789+Himesh000@users.noreply.github.com>
2025-05-12 09:47:28 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -5,3 +5,15 @@ let accountState;
 
 console.table([accountId , accountEmail , accountPassword , accountState])
 
+/*
+    NOTES:-
+    
+    - Use const for constants and let for variables.
+    - Avoid var – it’s function-scoped and lacks block scope.
+    - Declaring without a keyword creates unintended global variables (not recommended).
+    - var and let are initialized with undefined by default.
+    - const must be initialized during declaration or it will throw an error.
+    - Use console.table() to print data in a tabular format — more readable than console.log() for 
+      arrays and objects.
+*/
+
