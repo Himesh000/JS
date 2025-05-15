fdb316c911608aff79bf5bb3aa98023bd33b83d8
Himesh000 <138201789+Himesh000@users.noreply.github.com>
2025-05-15 10:41:00 +0000

    Arrays and Objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,41 @@
+// Note - Creating an object using object literals does not create a singleton, but using a constructor function or class with singleton pattern can create a singleton object.
+
+// singleton - A singleton is a design pattern that ensures only one instance of a class or object exists throughout the application.
+
+// Object.create 
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
